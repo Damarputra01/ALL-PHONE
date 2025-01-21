0195f806054970ce1288c8b5b9940fd700b0fe17
Damarputra01 <153520822+Damarputra01@users.noreply.github.com>
2025-01-22 02:02:05 +0800

    Add files via upload

---
+++ slider.php
@@ -0,0 +1,20 @@
+<div id="header-carousel" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active" style="height: 410px;">
+            <img class="img-fluid" src="<?php echo base_url('assets/home/img/Banner-1.png'); ?>" alt="Image">
+        </div>
+        <div class="carousel-item" style="height: 410px;">
+            <img class="img-fluid" src="<?php echo base_url('assets/home/img/Banner-2.png'); ?>" alt="Image">
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#header-carousel" data-slide="prev">
+        <div class="" style="width: 45px; height: 45px;">
+            <span class="carousel-control-prev-icon mb-n2"></span>
+        </div>
+    </a>
+    <a class="carousel-control-next" href="#header-carousel" data-slide="next">
+        <div class="" style="width: 45px; height: 45px;">
+            <span class="carousel-control-next-icon mb-n2"></span>
+        </div>
+    </a>
+</div>
